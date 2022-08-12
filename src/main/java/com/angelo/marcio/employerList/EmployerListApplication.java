9740aa5a350f15375554bee23640e936e84c53dd
Angelo <ampsf@icomp.ufam.edu.br>
2022-08-11 22:11:23 -0400

    init

---
+++ src/main/java/com/angelo/marcio/employerList/EmployerListApplication.java
@@ -0,0 +1,12 @@
+package com.angelo.marcio.employerList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployerListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployerListApplication.class, args);
+	};
+};
---
+++ src/test/java/com/angelo/marcio/employerList/EmployerListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelo.marcio.employerList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployerListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
